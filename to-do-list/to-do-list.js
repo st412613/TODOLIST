7fe3d85d96ed013cbbc5a2964bf1b8d72cb0458a
Shivam <110303492+st412613@users.noreply.github.com>
2023-03-22 11:47:55 +0530

    Add files via upload

---
+++ to-do-list/to-do-list.js
@@ -0,0 +1,69 @@
+let list = [];
+const btn=document.getElementById('push');
+const btnValue=document.getElementById('push').innerText;
+const recordDisplay = document.querySelector('.task');
+const listValue = localStorage.getItem('user');
+let edit_id = null;
+if (listValue != null) {
+    list = JSON.parse(listValue);
+}
+Display();
+document.querySelector('#push').onclick =
+    function () {
+        if (document.querySelector('.did input').value.length == 0) {
+            alert("Please Enter a Task");
+        }
+        else{
+        const Data = document.querySelector(".did input").value;
+        if (edit_id != null) {
+            list.splice(edit_id, 1, { 'name': Data });
+            edit_id = null;
+
+        } else {
+            list.push({ 'name': Data });
+        } 
+        Save(list);
+        Display();
+        btn.innerText=btnValue;
+        document.querySelector('.did input').value = "";
+
+    }
+
+    }
+function Save(list) {
+    const str = JSON.stringify(list)
+    localStorage.setItem('user', str)
+}
+
+function Display() {
+    let statement = ' ';
+    list.forEach((user, i) => {
+        statement += `<div>
+            <div id="do">
+            <div id="task">
+           <h4> ${user.name} </h4>
+             </div>
+             <div id="new">
+                <button class="edit" onclick='EditInfo(${i})'>Edit</button>
+                <button class="delete" onclick='DeleteInfo(${i})'>Delete</button>
+               </div>
+            </div>
+          </div>`;
+
+    });
+    recordDisplay.innerHTML = statement;
+
+}
+function DeleteInfo(id) {
+    list.splice(id, 1);
+    Save(list);
+    Display();
+}
+function EditInfo(id) {
+    edit_id = id;
+    document.querySelector('.did input').value = list[id].name;
+    list.splice(id, 1, recordDisplay);
+    btn.innerText="UPDATE";
+    Save(list);
+
+}
